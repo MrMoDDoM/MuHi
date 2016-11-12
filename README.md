cd9210df772cea83f3b33fa3d1fa3be9998aa89e
MrMoDDoM <danielebarattieri@gmail.com>
2016-11-12 12:05:22 +0100

    Licens and copyright terms updated

--- README.md
+++ README.md
@@ -1,21 +1,16 @@
 ///////////////////////////////////////////////////////////////
-//                     MuHi -
-//   Multifunctionals E(/Hi)yes traking
+//                     MuHi
+//         Multifunctionals E(/Hi)yes
 //
 //   @author: Daniele Barattieri di San Pietro
 //   @email : danielebarattieri[at]gmail.com
+//   @copyright: 2016
 //
 //  ----------------------------------------------------------
 //  This project is hosted and supported by the µHack assosiation,
 //  with love from Brescia, Italy ( www.muhack.org ) ;)
 //  The aim of this project is to provide a software assistance for
-//  tetraplegic, using a webcam to track the eyes moviments.
-//  The first part, where the sw find-track pupils, take care of
-//  "trigger areas", so when the user look outside the screen, the sw
-//  sets to true the corrispondig area-trigger.
-//
-//  The API provided for futher develop are composed only by these
-//  triggers and some functions for quality check.
+//  tetraplegic, using a webcam to detect the eyes' blinks.
 //  ----------------------------------------------------------
 //
 //  This Software is distribuited under the GNU/GPL license
@@ -23,3 +18,20 @@
 //  file attached to the source code
 //
 ////////////////////////////////////////////////////////////////
+    
+    This file is part of MuHi.
+    Copyright (C) 2016  Daniele Barattieri di San Pietro
+
+    MuHi is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MuHi.  If not, see <http://www.gnu.org/licenses/> or 
+    <http://www.muhack.org/> for futher datails.
--- src/MuHi.h
+++ src/MuHi.h
@@ -1,9 +1,51 @@
+///////////////////////////////////////////////////////////////
+//                     MuHi
+//         Multifunctionals E(/Hi)yes
+//
+//   @author: Daniele Barattieri di San Pietro
+//   @email : danielebarattieri[at]gmail.com
+//   @copyright: 2016
+//
+//  ----------------------------------------------------------
+//  This project is hosted and supported by the µHack assosiation,
+//  with love from Brescia, Italy ( www.muhack.org ) ;)
+//  The aim of this project is to provide a software assistance for
+//  tetraplegic, using a webcam to detect the eyes' blinks.
+//  ----------------------------------------------------------
+//
+//  This Software is distribuited under the GNU/GPL license
+//  Fot more informations, please see the LICENSE and README
+//  file attached to the source code
+//
+////////////////////////////////////////////////////////////////
+
+//============================================================================
+// Name        : MuHi.cpp
+// Author      : MrMoDDoM
+// Version     : 0.5
+// Copyright   : GNU/GPL
+// Description : MuHi in C++, Ansi-style
+//============================================================================
+
 /*
- * MiHi.h
- *
- *  Created on: Apr 28, 2016
- *      Author: mrmoddom
- */
+    This file is part of MuHi.
+    Copyright (C) 2016  Daniele Barattieri di San Pietro
+
+    MuHi is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MuHi.  If not, see <http://www.gnu.org/licenses/> or
+    <http://www.muhack.org/> for futher datails.
+
+*/
 
 #ifndef MUHI_H_
 #define MUHI_H_
